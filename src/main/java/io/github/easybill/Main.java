50f22dbc348038f077d2db2ce2f7a6a7f50d046c
Jan Nöhles <jan.noehles@easybill.de>
2024-06-10 10:55:22 +0200

    Fix codestyle

--- src/main/java/io/github/easybill/Main.java
+++ src/main/java/io/github/easybill/Main.java
@@ -2,7 +2,6 @@ package io.github.easybill;
 
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
-
 import java.nio.charset.StandardCharsets;
 
 @QuarkusMain
@@ -19,7 +18,9 @@ public class Main {
                 throw new Exception("could not read banner file");
             }
 
-            System.out.println(new String(bannerStream.readAllBytes(), StandardCharsets.UTF_8));
+            System.out.println(
+                new String(bannerStream.readAllBytes(), StandardCharsets.UTF_8)
+            );
         }
     }
 }
