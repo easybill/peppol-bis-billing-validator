a2352474a0bdacb0bfc08f317ad38d465ab80bd8
Jan Nöhles <jan.noehles@easybill.de>
2024-06-10 10:47:26 +0200

    Use openjdk-slim Add banner text Add example for JVM tool options

--- src/main/java/io/github/easybill/Main.java
+++ src/main/java/io/github/easybill/Main.java
@@ -7,6 +7,17 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        Main.printBannerText();
         Quarkus.run(args);
+    }
+
+    private static void printBannerText() throws Exception {
+        try (var bannerStream = Main.class.getResourceAsStream("/banner.txt")) {
+            if (bannerStream == null) {
+                throw new Exception("could not read banner file");
+            }
+
+            System.out.println(new String(bannerStream.readAllBytes()));
+        }
     }
 }
